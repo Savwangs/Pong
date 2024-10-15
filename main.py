6e1fdba8449ffa29a9c29fa7f592ca0045b6d792
Savir Wangoo <savirwangoo@Savirs-MacBook-Air.local>
2024-10-15 14:23:21 -0700

    entry point of the application that imports the frontend class and runs the program

--- main.py
+++ main.py
@@ -1,30 +1,11 @@
-from src.memory import Memory
-from src.llm import LLM
-from src.frontend import Frontend
-import os
-from dotenv import load_dotenv
-import requests
-from twilio.rest import Client
+from frontend import Frontend
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Get the OpenAI API key from environment variables
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
-TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID","")
-TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN","")
-TWILIO_PHONE_NUMBER = os.environ.get("TWILIO_PHONE_NUMBER","")
-
-twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-
-# Instantiate the classes
-memory = Memory()
-llm = LLM(api_key=OPENAI_API_KEY)
-frontend = Frontend(memory=memory, llm=llm, twilio_client = twilio_client)
-
-# Run the frontend to start the chatbot
-if __name__ == "__main__":
+def main():
+    frontend = Frontend()
     frontend.run()
+
+if __name__ == "__main__":
+    main()
 
 
         

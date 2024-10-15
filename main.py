4802763d0ddd35462a32b261b7b0d6d4942292a6
Savir Wangoo <savirwangoo@Savirs-MacBook-Air.local>
2024-10-15 10:09:05 -0700

    entry point of the application that imports the classes and runs the application

--- main.py
+++ main.py
@@ -4,31 +4,27 @@ from src.frontend import Frontend
 import os
 from dotenv import load_dotenv
 import requests
+from twilio.rest import Client
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Get the OpenAI API key from environment variables
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID","")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN","")
+TWILIO_PHONE_NUMBER = os.environ.get("TWILIO_PHONE_NUMBER","")
 
-try:
-    response = requests.get("https://api.openai.com")
-    if response.status_code == 200:
-        print("API is accessible")
-    else:
-        print("Failed to access API")
-except requests.ConnectionError:
-    print("Network issue: Unable to connect to OpenAI API")
+twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 # Instantiate the classes
 memory = Memory()
 llm = LLM(api_key=OPENAI_API_KEY)
-frontend = Frontend(memory=memory, llm=llm)
+frontend = Frontend(memory=memory, llm=llm, twilio_client = twilio_client)
 
 # Run the frontend to start the chatbot
 if __name__ == "__main__":
     frontend.run()
-
 
 
         
